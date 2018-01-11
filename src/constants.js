1eacdfc49343917b4c6dd21b3946f89df5421d76
Chris Laughlin <chris_laughlin@rapid7.com>
2018-01-11 00:28:38 +0000

    build process

--- src/constants.js
+++ src/constants.js
@@ -4,12 +4,3 @@ export const URL = 'https://github.com/';
 export const REMOTES_CMD = 'git remotes';
 export const BRANCH_NAME = 'git branch | grep \\* | cut -d \' \' -f2';
 export const PUSH_BRANCH = 'git push -u origin';
-
-// module.exports = {
-//     START,
-//     END,
-//     URL,
-//     REMOTES_CMD,
-//     BRANCH_NAME,
-//     PUSH_BRANCH
-// };
